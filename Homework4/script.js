699e7fbf620b48e02ad5e2d79f5ccfd6c2837538
KirillEvgen <churkin.ke@gmail.com>
2025-01-28 19:04:28 +0500

    Сделал задания по js

---
+++ Homework4/script.js
@@ -0,0 +1,54 @@
+// Задание 1 
+let a = 10
+alert(a);
+alert(a=20);
+
+// Задание 2
+let iphone = 2007
+alert(iphone);
+
+// Задание 3
+let jscreator = "Brendan Eich"
+alert (jscreator);
+
+// Задание 4
+let q = 10
+let w = 2
+alert (q + w)
+alert (q - w)
+alert (q * w)
+alert (q / w)
+
+// Задание 5
+alert(w ** 5)
+
+// Задание 6
+a = 9
+b = 2
+alert(a % b)
+
+// Задание 7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+// Задание 8
+let age = Number( prompt("Сколько вам лет?"));
+alert (age);
+
+
+// Задание 9
+let user = {
+    name: "Кирилл",    
+    age: 26,            
+    isAdmin: true };
+    console.log(user)
+
+// Задание 10
+let Whatname = prompt("Как вас зовут?")
+alert(Whatname)
